907898652c1f8a2d3e01288206cd84964b796508
Oblique000 <neomathe000@gmail.com>
2024-01-19 04:54:51 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,10 +3,9 @@ import sys
 
 if __name__ == "__main__":
     len_of_argv = len(sys.argv) - 1
+
     if len_of_argv >= 1:
-        count = 0
-        for i in range(1, len(sys.argv)):
-            count = count + int(sys.argv[i])
-        print("{}".format(count))
+        total = sum(int(arg) for arg in sys.argv[1:])
+        print(total)
     else:
         print(0))
