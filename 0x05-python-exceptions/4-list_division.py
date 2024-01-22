6f991c392020b27bea176db24e8d464b8e4dcb1a
Oblique000 <neomathe000@gmail.com>
2024-01-22 10:52:56 -0800

    Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,7 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             result = 0
             print("wrong type")
-        else:
+        finally:
             new_list.append(result)
 
     return new_list
