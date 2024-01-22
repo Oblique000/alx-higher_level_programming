e86abca36c5eb32bb4b0caf87a7896520777bd54
Oblique000 <neomathe000@gmail.com>
2024-01-22 11:49:06 -0800

    Safe integer print with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError) as e:
-        print("Exception: {e}", file=sys.stderr)
+        print(f"Exception: {e}", file=sys.stderr)
         return (False)
