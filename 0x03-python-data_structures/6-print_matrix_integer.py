b715563bd302cb1f4a3d4a87e76eff9f07b7b433
Oblique000 <neomathe000@gmail.com>
2024-01-22 02:00:59 -0800

    Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
+    if not matrix:
         print()
     else:
-        for row in matrix:
-            print(' '.join(map(str, row)))
+        for row in range(len(matrix)):
+            for item in range(len(matrix[row])):
+                if item != len(matrix[row]) - 1:
+                    endspace = ' '
+                else:
+                    endspace = ''
+                print("{:d}".format(matrix[row][item]), end=endspace)
+            print()
