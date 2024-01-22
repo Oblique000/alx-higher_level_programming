0635c64e3bc7c87902f7fe5e06d91a96ad8d8d92
Oblique000 <neomathe000@gmail.com>
2024-01-22 03:49:16 -0800

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,14 +10,14 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
+    listint_t *new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
 
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
+    new->n = n;
+    new->next = *head;
+    *head = new;
+    return (new);
 }
 
 /**
@@ -27,31 +27,31 @@ listint_t *add_nodeint(listint_t **head, const int n)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *aux = NULL, *aux2 = NULL;
+    listint_t *aux = NULL, *aux2 = NULL;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+    if (*head == NULL || (*head)->next == NULL)
+        return (1);
 
-	while (*head != NULL)
-	{
-		add_nodeint(&aux, (*head)->n);
-		*head = (*head)->next;
-	}
+    while (*head != NULL)
+    {
+        add_nodeint(&aux, (*head)->n);
+        *head = (*head)->next;
+    }
 
-	aux2 = aux;
-	*head = aux;
+    aux2 = aux;
+    *head = aux;
 
-	while (*head != NULL)
-	{
-		if ((*head)->n != aux2->n)
-		{
-			free_listint(aux);
-			return (0);
-		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
-	}
+    while (*head != NULL)
+    {
+        if ((*head)->n != aux2->n)
+        {
+            free_listint(aux);
+            return (0);
+        }
+        *head = (*head)->next;
+        aux2 = aux2->next;
+    }
 
-	free_listint(aux);
-	return (1);
+    free_listint(aux);
+    return (1);
 }
