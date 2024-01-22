d45d8d86b0326550384f21c578c4dc15a23794be
Oblique000 <neomathe000@gmail.com>
2024-01-22 02:28:54 -0800

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        sentence = None
-        if sentence:
-            sen_len = len(sentence)
-        else:
-            sen_len = 0
-            return (sen_len, sentence if not sentence else sentence[:1])
+    sen_len = len(sentence) if sentence else 0
+    return sen_len, sentence[:1] if sentence else None
