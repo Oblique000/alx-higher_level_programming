4ecc23c8cb340b11f22d5e668786cdac6d444145
Oblique000 <neomathe000@gmail.com>
2024-01-22 11:18:36 -0800

    Safe integer print with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
+        if not isinstance(value, int):
+            raise TypeError("Value must be an integer")
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError) as e:
