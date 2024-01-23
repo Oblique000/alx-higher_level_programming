79f24dd43df336939bbae12bd3c63e5458ca6563
Oblique000 <neomathe000@gmail.com>
2024-01-23 11:26:38 -0800

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
-    """The class Square defines a square."""
+    """A class Square that represent a square."""
 
     def __init__(self, size=0):
-        """Initialize the new square.
-
+        """Initialize a new square.
         Args:
-            size (int): The size of a new square.
+            size (int): The size of the new square.
         """
-        if not (isinstance(size, int) and size >= 0):
-            raise ValueError("size must be a non-negative integer")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        """This returns the area of the Square.
-
-        Returns:
-            int: Area of the square.
-        """
-        return self.__size ** 2
-
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
