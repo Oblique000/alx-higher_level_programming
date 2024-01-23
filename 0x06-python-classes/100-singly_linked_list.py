e45fdfdcd7fedc88ca07d83dc27e5eea4227d0cd
Oblique000 <neomathe000@gmail.com>
2024-01-23 12:29:53 -0800

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
+"""100-singly_linked_list.py"""
+
 
 class Node:
-    """Class representing a node in a singly linked list."""
+    """This is a class node that contains a data and the next node."""
     def __init__(self, data, next_node=None):
-        """Initialize a new node.
+        """the spical method initializes the data and next_node.
 
         Args:
-            data (int): The value of the node.
-            next_node (Node): The next node in the list.
+            data (int): the value of the node.
+            next_node (Node): the address of the next node.
         """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Getter and setter for the node's data."""
+        """The the getter and setter for data.
+
+        Args:
+            value (int): the value to be set for data.
+        """
         return self.__data
 
     @data.setter
@@ -25,7 +31,11 @@ class Node:
 
     @property
     def next_node(self):
-        """Getter and setter for the next node."""
+        """The getter and setter for next_node.
+
+        Args:
+            value (Node): the value to be set for next_node.
+        """
         return self.__next_node
 
     @next_node.setter
@@ -36,31 +46,45 @@ class Node:
 
 
 class SinglyLinkedList:
-    """Class representing a singly linked list."""
+    """This class contains the implementation for SLL."""
     def __init__(self):
-        """Initialize an empty singly linked list."""
-        self.head = None
+        """It initalizes the head.
+
+        Args:
+            head: the head of the linked list.
+        """
+        self.__head = None
 
     def sorted_insert(self, value):
-        """Insert a new node with a given value in sorted order."""
+        """Inserts is new_node to the list in increasing order.
+
+        Args:
+            new_node: is the new created node
+            temp: holds the self.__head.
+        """
         new_node = Node(value)
 
-        if not self.head or value < self.head.data:
-            new_node.next_node = self.head
-            self.head = new_node
+        if self.__head is None or value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
             return
 
-        temp = self.head
-        while temp.next_node and temp.next_node.data < value:
+        temp = self.__head
+        while temp.next_node is not None and temp.next_node.data < value:
             temp = temp.next_node
 
         new_node.next_node = temp.next_node
         temp.next_node = new_node
 
     def __str__(self):
-        """Return a string representation of the linked list."""
+        """returns the list in string format.
+
+        Args:
+            result: is a str that hold the the SLL that will be returned
+            temp: holds the self.__head.
+        """
         result = ""
-        temp = self.head
+        temp = self.__head
         while temp:
             result += str(temp.data)
             if temp.next_node:
