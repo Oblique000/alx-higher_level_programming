f2fa3e73c3d98817e835e70e790ef84300b21782
Oblique000 <neomathe000@gmail.com>
2024-02-01 07:25:06 -0800

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
 This is the "0-add_integer" module.
-It returns an integer.
+It returns an intger.
 """
 
 
 def add_integer(a, b=98):
-    """
-    This function adds the sum of two numbers which
+    """This function adds the sum of two number which
     are of types float or int.
 
     Args:
@@ -16,18 +15,18 @@ def add_integer(a, b=98):
     Return:
         result (int): holds the sum of a and b.
     """
-    if not isinstance(a, (int, float)):
+    if ((type(a) is not int) and (type(a) is not float)):
         raise TypeError("a must be an integer")
 
     if a is None:
         raise TypeError("a must be an integer")
 
-    if not isinstance(b, (int, float)):
+    if ((type(b) is not int) and (type(b) is not float)):
         raise TypeError("b must be an integer")
 
     result = a + b
 
-    if result in [float('inf'), float('-inf')]:
+    if result == float('inf') or result == float('-inf'):
         raise OverflowError("float overflow")
 
     result = int(a) + int(b)
