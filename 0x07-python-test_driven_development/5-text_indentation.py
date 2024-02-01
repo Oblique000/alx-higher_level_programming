9ae8cc501e1b57d77e9b98a4a51ac77e35f767cd
Oblique000 <neomathe000@gmail.com>
2024-02-01 14:28:25 -0800

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,43 @@
 #!/usr/bin/python3
 """
-Defines a text-indentation function.
+prints a text with 2 new lines after each
+of these characters: ., ? and :.
+5-text_indentation.py
 """
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'
-    
+    """prints a text with 2 new lines after
+    each of these characters: ., ? and :.
+
+    Args:
+        char(list) : containing the characters.
+        x(int): keeps track of the number of loops
+        lines(list): list containing the splitted string
+
     Raises:
-        TypeError: If text is not a string.
+        TypeError: text must be a string
+        ValueError: input must not be an empty string
     """
-    if not isinstance(text, str):
+    if type(text) is not str or text is None:
         raise TypeError("text must be a string")
+    if not text.strip():
+        raise ValueError("input must not be an empty string")
+
+    char_list = ['.', '?', ':']
+
+    for char in char_list:
+        text = text.replace(char + ' ', char + '\n')
+
+    lines = text.split('\n')
 
     x = 0
-    while x < len(text) and text[x] == ' ':
-        x += 1
 
-    while x < len(text):
-        print(text[x], end="")
-        if text[x] == "\n" or text[x] in ".?:":
-            if text[x] in ".?:":
-                print("\n")
-            x += 1
-            while x < len(text) and text[x] == ' ':
-                x += 1
-            continue
+    for line in lines:
+        stripped_line = line.strip()
         x += 1
+        if stripped_line and x < len(lines):
+            print(stripped_line)
+            print()
+        else:
+            print(stripped_line, end="")
