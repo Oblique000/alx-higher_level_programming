16ff4b81f7a7ed905cedd2e2e61fc853c510bd97
Oblique000 <neomathe000@gmail.com>
2024-02-01 11:51:10 -0800

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
 """
-say_my_name: This module prints My name is <first name> <last name>.
+3-say_my_name.
+This module prints My name is <first name> <last name>.
 """
+
 
 def say_my_name(first_name, last_name=""):
     """
-    Prints My name is <first name> <last name>.
+    This function prints My name is <first name> <last name>.
 
     Raises:
-        TypeError: If first_name or last_name is not a string containing letters.
+        TypeError: first_name must be a string.
+        TypeError: last_name must be a string.
     """
-    if not isinstance(first_name, str) or not first_name.isalpha():
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if (
+            first_name and not
+            all(char.isalpha() or char.isspace() for char in first_name)
+            ):
         raise TypeError("first_name must be a string containing letters")
 
-    if last_name and (not isinstance(last_name, str) or not last_name.isalpha()):
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    if (
+            last_name and not
+            all(char.isalpha() or char.isspace() for char in last_name)
+            ):
         raise TypeError("last_name must be a string containing letters")
 
     print("My name is {} {}".format(first_name, last_name))
