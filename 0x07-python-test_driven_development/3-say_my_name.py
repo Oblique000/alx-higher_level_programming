c0da4fdf0a1e65dea86105bf5c61f4fc215cbf7f
Oblique000 <neomathe000@gmail.com>
2024-02-01 11:49:39 -0800

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Defines a name-printing function."""
-
+"""
+say_my_name: This module prints My name is <first name> <last name>.
+"""
 
 def say_my_name(first_name, last_name=""):
-    """Print a name.
-
-    Args:
-        first_nam.
-    Raises:
-        TypeError: If either of first_name or last_name are not strings.
     """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
+    Prints My name is <first name> <last name>.
+
+    Raises:
+        TypeError: If first_name or last_name is not a string containing letters.
+    """
+    if not isinstance(first_name, str) or not first_name.isalpha():
+        raise TypeError("first_name must be a string containing letters")
+
+    if last_name and (not isinstance(last_name, str) or not last_name.isalpha()):
+        raise TypeError("last_name must be a string containing letters")
+
     print("My name is {} {}".format(first_name, last_name))
