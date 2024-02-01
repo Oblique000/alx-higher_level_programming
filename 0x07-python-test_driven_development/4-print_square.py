d0385896596da7fa642162486dfb5c6ca797e013
Oblique000 <neomathe000@gmail.com>
2024-02-01 11:59:26 -0800

    Print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-"""Defines a square-printing function."""
-
+"""
+4-print_square.py
+This module prints a square with the character #
+"""
 
 def print_square(size):
-    """Print a square with the # character.
-
-    Args:
-        size (int): The height/wiror: If size is < 0
     """
-    if not isinstance(size, int):
+    Prints a square with the character #.
+    
+    Raises:
+        TypeError: If size is not an integer or a negative float.
+        ValueError: If size is less than 0.
+    """
+    if size is None:
+        raise TypeError("missing one argument - size")
+
+    if not isinstance(size, int) or (isinstance(size, float) and size < 0):
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
-        [print("#", end="") for j in range(size)]
-        print("")
+    for _ in range(size):
+        print("#" * size)
+
