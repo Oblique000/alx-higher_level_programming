d7fd7a996b779735f1350e0a56a63d7e5cac891b
Oblique000 <neomathe000@gmail.com>
2024-02-06 11:01:55 -0800

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+class Student:
+    """A class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """The class constructor.
+        Args:
+            first_name: the first name of the student
+            last_name: the last name of the student
+            age: the age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Public method that retrieves a dictionary representation
+        of a Student instance.
+        """
+        result = {key: getattr(self, key) for key in self.__dict__} if attrs is None else {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return result
+
+    def reload_from_json(self, json):
+        """Public method that replaces all attributes of the Student instance
+        Args:
+            json: json will always be a dictionary
+        """
+        {setattr(self, key, value) for key, value in json.items()}
