b271ab774095f19e7bceac7802329162ab454c9c
Oblique000 <neomathe000@gmail.com>
2024-02-06 07:50:27 -0800

    Class tp JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,7 +4,7 @@
 
 def class_to_json(obj):
     """Returns the dictionary description with simple data
-    structure (list, dictionary, string, integer, and boolean)
+    structure (list, dictionary, string, integer and boolean)
     for JSON serialization of an object.
     Args:
         obj: is an instance of a Class.
@@ -12,14 +12,4 @@ def class_to_json(obj):
         dict: returns the dictionary description.
     """
     if hasattr(obj, '__dict__'):
-        return {key: value for key, value in obj.__dict__.items() if is_serializable(value)}
-    return {}
-
-
-def is_serializable(value):
-    """Check if the value is serializable."""
-    try:
-        json.dumps(value)
-        return True
-    except (TypeError, OverflowError):
-        return False
+        return obj.__dict__
