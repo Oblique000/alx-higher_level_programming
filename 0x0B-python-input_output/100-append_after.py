5a5f7daceb5dc569d347e7a7fdf48c1dd0081f95
Oblique000 <neomathe000@gmail.com>
2024-02-06 11:15:32 -0800

    Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
-"""Module for appending lines after a specific string in a file."""
+"""100-append_after module"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """
-    Inserts a line of text to a file, after each line containing
+    """Inserts a line of text to a file, after each line containing
     a specific string.
 
     Args:
-        filename (str): The name of the file.
-        search_string (str): The string to search for.
-        new_string (str): The new string to be added to the file.
+        filename: the name of the file
+        search_string: the string to search for
+        new_string: the new string to be added to the test file
     """
-    try:
-        with open(filename, "r", encoding='utf-8') as file:
-            lines = file.readlines()
+    with open(filename, "r", encoding='utf-8') as file:
+        lines = file.readlines()
 
-        with open(filename, "w", encoding='utf-8') as file:
-            for line in lines:
-                file.write(line)
-                if search_string in line:
-                    file.write(new_string)
-
-        print(f"Lines appended successfully after '{search_string}' in '{filename}'.")
-    except FileNotFoundError:
-        print(f"Error: File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-# Example usage:
-# append_after("example.txt", "specific_string", "new_line_to_add\n")
+    with open(filename, "w", encoding='utf-8') as file:
+        for row in lines:
+            file.write(row)
+            if search_string in row:
+                file.write(new_string)
