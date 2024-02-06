976c283890f5a6ce76973dc6751351276b5032f5
Oblique000 <neomathe000@gmail.com>
2024-02-06 07:12:29 -0800

    Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""2-append_write module"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file (UTF8) and
+    returns the number of characters added.
+    Args:
+        filename: the file to be opened
+        text: what is to be appended to the file
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
